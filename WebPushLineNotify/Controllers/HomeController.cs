4b30cdd12ddcf8180896cb1d6dd8b5324d480581
taotaan <pariidd09@gmail.com>
2025-01-25 22:32:38 +0700

    Add project files.

---
+++ WebPushLineNotify/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebPushLineNotify.Models;
+
+namespace WebPushLineNotify.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult LineNotify(NotifyViewModel model)
+        {
+            return View(model);
+        }
+    }
+}
---
+++ WebPushLineNotify/Models/NotifyViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebPushLineNotify.Models
+{
+    public class NotifyViewModel
+    {
+        public string Message { get; set; }
+    }
+}
